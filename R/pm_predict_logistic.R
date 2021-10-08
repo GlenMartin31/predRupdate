9b19a5b7a93b330db142fc763fc8a159aea437b0
Glen Martin (home) <glen.martin@manchester.ac.uk>
2021-10-08 12:57:31 +0100

    Check DM and existingcoef are conformable

--- R/pm_predict_logistic.R
+++ R/pm_predict_logistic.R
@@ -1,5 +1,10 @@
 pm_predict_logistic <- function(existingcoefs,
                                 DM) {
+  #Input checking
+  if (ncol(DM) != length(existingcoefs)) {
+    stop("Existing coefficients of the model and new data to make predictions on are non-conformable",
+         call. = FALSE)
+  }
   #Calculate the linear predictor
   LP <- as.numeric(DM %*% existingcoefs)
   #Map to predicted risks
