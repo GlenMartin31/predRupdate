1b2476753d5d45d32807af90424fb44574e53230
Glen Martin (home) <glen.martin@manchester.ac.uk>
2021-12-08 10:50:15 +0000

    move pre-processing steps to seperate internal function

--- R/pm_input_info.R
+++ R/pm_input_info.R
@@ -19,7 +19,7 @@
 #'   character string that can be coerced to that class). This specifies the
 #'   functional form description of the existing prediction model. The details
 #'   of the model specification are given under "Details".
-#' @param newdata  data frame which will be used to make predictions on using
+#' @param newdata  data.frame which will be used to make predictions on using
 #'   the existing prediction model. Variable names must match those in
 #'   \code{existingcoefs} and \code{formula}, after applying any pre-processing
 #'   steps (using \code{pre_processing}) where needed. See "Details".
@@ -354,91 +354,26 @@ pm_input_info <- function(model_type = c("logistic", "survival"),
 
   ##Define the design matrix, with/without pre-processing steps as defined by
   ##pre_processing:
-  if (is.null(pre_processing)) {
-    #If no pre_processing steps are specified, check that all predictor
-    #variables specified in 'formula' are also in 'newdata'
-    if (all(all.vars(formula) %in% names(newdata)) == FALSE) {
-      stop("Ensure that all predictor variables specified in 'formula' are also in 'newdata'",
-           call. = FALSE)
-    }
-    #Define a design matrix given the provided functional form of the existing
-    # model; if model is survival, no intercept should be created in DM
-    if (model_type == "survival") {
-      DM <- stats::model.matrix(formula, newdata)
-      if (length(which(colnames(DM) == "(Intercept)")) != 0) {
-        DM <- DM[ ,-which(colnames(DM) == "(Intercept)"), drop=FALSE]
-      }
-    } else {
-      DM <- stats::model.matrix(formula, newdata)
-    }
+  if (!is.null(pre_processing)) {
+    newdata <- apply_pre_processing(newdata = newdata,
+                                    pre_processing = pre_processing)
+  }
 
-  } else{
-    #Run some input checks on structure of user-supplied 'pre_processing'
-    if (!is.list(pre_processing)) {
-      stop("'pre_processing' should be a list", call. = FALSE)
-    }
-    if (all(sapply(pre_processing,
-                   is.function)) == FALSE){
-      stop("'pre_processing' should be a list where each element of the list is a function", call. = FALSE)
-    }
-
-    #Apply the list of transformation/pre-processing steps to the dataset. Will
-    #check that the user-supplied functions in 'pre_processing' can be evaluated
-    #safely. Will also check that results from functions in 'pre_processing' are
-    #of the correct length/dim to merge with newdata:
-    transformed_vars <- tryCatch(
-      {
-        lapply(pre_processing, function(f) f(newdata))
-      },
-      error = function(cond){
-        stop(paste("Some elements of pre_processing causes the following error: \n", cond),
-             call. = FALSE)
-      },
-      warning = function(cond){
-        stop(paste("Some elements of pre_processing causes the following warning: \n", cond),
-             call. = FALSE)
-      }
-    )
-    #Check if all of the functions within pre_processing return empty/no values,
-    #then proceed without any pre-processing steps
-    if (all(lapply(transformed_vars, length) == 0) == TRUE) {
-      warning("All functions supplied in pre_processing return empty results - proceeding without pre-processing data.")
-    } else{
-      #check whether any of the functions within pre_processing return empty/no
-      #values, returning a warning if so
-      if (any(lapply(transformed_vars, length) == 0) == TRUE) {
-        warning("Some functions supplied in pre_processing return empty result - removing these steps from pre-processing.")
-      }
-
-      #convert the results of the pre_processing functions to be a list where
-      #each element is a transformed/pre-processed variable:
-      transformed_vars <- lapply(rapply(transformed_vars, enquote, how="unlist"),
-                                 eval)
-      #Test to make sure that each transformed/pre-processed variable is the
-      #correct length to column-bind with newdata
-      if (any(sapply(transformed_vars, length) != nrow(newdata))) {
-        stop("Length of output returned by some elements of 'pre_processing' does not match nrow(newdata)",
-             call. = FALSE)
-      }
-      #Merge the transformation/pre-processing variables into the dataset
-      newdata <- cbind(newdata, transformed_vars)
-    }
-
-
-    #Now pre-processing has been completed, check that all predictor
-    #variables specified in 'formula' are also in 'newdata'
-    if (all(all.vars(formula) %in% names(newdata)) == FALSE) {
-      stop("Ensure that all predictor variables specified in 'formula' are also in 'newdata' after pre-processing steps",
-           call. = FALSE)
-    }
-    #Define a design matrix given the provided functional form of the existing
-    # model; if model is survival, no intercept should be created in DM
-    if (model_type == "survival") {
-      DM <- stats::model.matrix(formula, newdata)
+  #Check that all predictor variables specified in 'formula' are also included
+  #in the 'newdata'
+  if (all(all.vars(formula) %in% names(newdata)) == FALSE) {
+    stop("Ensure that all predictor variables specified in 'formula' are also in 'newdata'",
+         call. = FALSE)
+  }
+  #Define a design matrix given the provided functional form of the existing
+  # model; if model is survival, no intercept should be created in DM
+  if (model_type == "survival") {
+    DM <- stats::model.matrix(formula, newdata)
+    if (length(which(colnames(DM) == "(Intercept)")) != 0) {
       DM <- DM[ ,-which(colnames(DM) == "(Intercept)"), drop=FALSE]
-    } else {
-      DM <- stats::model.matrix(formula, newdata)
     }
+  } else {
+    DM <- stats::model.matrix(formula, newdata)
   }
 
 
